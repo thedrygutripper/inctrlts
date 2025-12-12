142196153b84bddd43c11b2eada2188998120048
thedrygutripper <thedrygutripper@gmail.com>
2025-12-12 18:52:25 -0500

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import HomepageMockup from "./index";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <HomepageMockup />
+  </React.StrictMode>
+);
