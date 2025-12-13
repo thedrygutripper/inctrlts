6d06960bdd9867e2517190a24fd681ca35421eba
thedrygutripper <thedrygutripper@gmail.com>
2025-12-13 15:21:15 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import HomepageMockup from "./index";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HomepageMockup />
+    <App />
   </React.StrictMode>
 );
