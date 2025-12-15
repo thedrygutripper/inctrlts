7b542d3d61478a6218334fa742f9888657032e05
thedrygutripper <thedrygutripper@gmail.com>
2025-12-14 21:36:21 -0500

    Create Services.jsx

---
+++ src/pages/Services.jsx
@@ -0,0 +1,24 @@
+export default function Services() {
+  return (
+    <main style={{
+      minHeight: "100vh",
+      background: "#0b0b0b",
+      color: "#fff",
+      paddingTop: "120px",
+      paddingLeft: "40px",
+      paddingRight: "40px"
+    }}>
+      <h1 style={{ fontSize: "40px", marginBottom: "30px" }}>
+        Our Services
+      </h1>
+
+      <ul style={{ lineHeight: "2", color: "#ccc" }}>
+        <li>Security Camera Installation</li>
+        <li>Structured Cabling (CAT6 / Fiber)</li>
+        <li>Digital Signage Systems</li>
+        <li>TV Mounting & AV Integration</li>
+        <li>IT Field Technician Services</li>
+      </ul>
+    </main>
+  );
+}
