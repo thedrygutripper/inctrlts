74a358464d36e4d89813bc463928a9666ff351de
thedrygutripper <thedrygutripper@gmail.com>
2025-12-14 21:34:56 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/services" element={<Services />} />
       </Routes>
     </BrowserRouter>
   );
