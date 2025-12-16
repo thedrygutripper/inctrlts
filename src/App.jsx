908e98675bc7a2fd460196bf1738c6ad24a93937
thedrygutripper <thedrygutripper@gmail.com>
2025-12-16 18:12:05 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Services from "./pages/Services";
-import About from "./pages/About";
 import Contact from "./pages/Contact";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <div className="bg-black text-white min-h-screen flex flex-col">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+
+      <main className="flex-grow">
+        <Home />
+      </main>
+
       <Footer />
-    </BrowserRouter>
+    </div>
   );
 }
